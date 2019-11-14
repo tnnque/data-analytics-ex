f0b72c4e48a22d2905dd3c73ffb3256bffde7f26
tnnque <tnnque@gmail.com>
2019-11-14 16:08:43 +0700

    Ch002 - WORKING

--- que/ch02.py
+++ que/ch02.py
@@ -124,3 +124,75 @@ names1880 = pd.read_csv('/Users/tnnque/PycharmProjects/data-analytics-ex/dataset
 years = range(1880, 2011)
 pieces = []
 columns = ['name', 'sex', 'births']
+for year in years:
+    path = '/Users/tnnque/PycharmProjects/data-analytics-ex/datasets/babynames/yob%d.txt' % year
+    frame = pd.read_csv(path, names=columns)
+
+    frame['year'] = year
+    pieces.append(frame)
+names = pd.concat(pieces, ignore_index=True)
+total_births = names.pivot_table('births', 'year', columns='sex', aggfunc=sum)
+# print(total_births.tail())
+import matplotlib.pyplot as plt
+total_births.plot(title='Total births by sex and year')
+# plt.show()
+def add_prop(group):
+    births = group.births.astype(float)
+    group['prop']= births/births.sum()
+    return group
+names = names.groupby(['year', 'sex']).apply(add_prop)
+# print(names)
+import numpy as np
+np.allclose(names.groupby(['year', 'sex']).prop.sum(), 1)
+#taketop1000 method1
+def get_top1000(group):
+    return group.sort_index(by='births', ascending=False)[:1000]
+grouped = names.groupby(['year', 'sex'])
+top1000 = grouped.apply(get_top1000)
+#taketop1000 method2
+pieces = []
+for year, group in names.groupby(['year','sex']):
+    pieces.append(group.sort_index(by='births', ascending=False)[:1000])
+top1000 = pd.concat(pieces, ignore_index=True)
+#Analyzing Naming Trends
+boys = top1000[top1000.sex == 'M']
+girls = top1000[top1000.sex == 'F']
+total_births = top1000.pivot_table('births', 'year', columns='name', aggfunc=sum)
+subset = total_births[['John', 'Harry', 'Mary', 'Marilyn']]
+subset.plot(subplots=True, figsize=(12, 10), grid=False, title='Number of births per year')
+
+table = top1000.pivot_table('prop', 'year', columns='sex', aggfunc=sum)
+table.plot(title='Sum of table1000.prop by year and sex', yticks=np.linspace(0, 1.2, 13), xticks=range(1880, 2020, 10))
+# plt.show()
+df = boys[boys.year == 2010]
+prop_cumsum = df.sort_index(by='prop', ascending=False).prop.cumsum()
+# print(prop_cumsum[:10])
+print(prop_cumsum.searchsorted(0.5))
+df = boys[boys.year == 1900]
+in1900 = df.sort_index(by='prop', ascending=False).prop.cumsum()
+print(in1900.searchsorted(0.5) + 1)
+
+def get_quantile_count(group, q=0.5):
+    group = group.sort_index(by='prop', ascending=False)
+    return group.prop.cumsum().searchsorted(q) + 1
+
+diversity = top1000.groupby(['year', 'sex']).apply(get_quantile_count)
+diversity = diversity.unstack('sex')
+# print(diversity.head())
+diversity.plot(title="Number of popular names in top 50%")
+# plt.show()
+# Extract last letter from name column
+get_last_letter = lambda x: x[-1]
+last_letters = names.name.map(get_last_letter)
+last_letters.name = 'last_letter'
+table = names.pivot_table('births', last_letters, columns=['sex','year'], aggfunc=sum)
+subtable = table.reindex(columns=[1910, 1960, 2010], level='year')
+print(subtable.sum())
+letter_prop = subtable/subtable.sum().astype(float)
+fig, axes = plt.subplots(2, 1, figsize=(10,8))
+letter_prop['M'].plot(kind='bar', rot=0, ax=axes[0], title='Male')
+letter_prop['F'].plot(kind='bar', rot=0, ax=axes[1], title='Female')
+
+letter_prop = table/table.sum().astype(float)
+dny_ts = letter_prop.ix[['d', 'n', 'y'], 'M'].T
+print(dny_ts.head())
