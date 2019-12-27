8e5e75abc39378e1d44d170da2d2303ab6fb4ebd
tnnque <tnnque@gmail.com>
2019-12-27 17:20:14 +0700

    MultiLinear1 - WORKING

--- que/MultiLinear1.py
+++ que/MultiLinear1.py
@@ -12,25 +12,26 @@ def extract(path, col_names):
     df = df[col_names]
     current_row = None
     old_row = None
-    sum_row = []
-    n = 0
-    Y = []
-    X = []
+    group_row = pd.DataFrame(columns=[col_names])
+    new_df =  pd.DataFrame(columns=[col_names])
     for index, row in df.iterrows():
         if index == 0:
             old_row = row
+            group_row.append(row)
+            n = 1
         else:
             if row['dayofweek'] == old_row['dayofweek']:
-                n += 1
                 old_row = row
+                group_row.append(row)
+                n += 1
             else:
-                x = []
-                x.append(df[:n])
-        X.extend(x)
-            # n = 1
-            # sum_row = row[col_names]
-        current_row = row
-
+                old_row = row
+                new_df.append(sum(group_row))
+                n = 1
+                del temp
+    Y = new_df.iloc[:, :1]
+    X = new_df.iloc[:, 1:]
+    return X, Y
 
 extract(path, col_names)
 #/Users/tnnque/PycharmProjects/data-analytics-ex/que/train_small_cleaned.csv
