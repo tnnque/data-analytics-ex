675f66e7ab50c39bbfa2ec2683dcddce5f9d6cd9
tnnque <tnnque@gmail.com>
2019-12-30 15:03:55 +0700

    MultiLinear1 - WORKING

--- que/MultiLinear1.py
+++ que/MultiLinear1.py
@@ -3,40 +3,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import DataFrame
 
-path = input("Enter path:")
-col_names = input("Enter dependent/independent variables:")
+path = '/Users/tnnque/PycharmProjects/data-analytics-ex/que/train_small_cleaned.csv'
+# input("Enter path:")
+col_names = 'Open SMA_50 SMA_20 dayofweek'
+# input("Enter dependent/independent variables:")
 
 def extract(path, col_names):
     df = pd.read_csv(path)
-    col_names = col_names.split(' ')
+    col_names = col_names.split()
     df = df[col_names]
     current_row = None
     old_row = None
-    group_row = pd.DataFrame(columns=[col_names])
-    new_df =  pd.DataFrame(columns=[col_names])
+    group_row = pd.DataFrame()
+    new_df =  pd.DataFrame()
     for index, row in df.iterrows():
         if index == 0:
             old_row = row
-            group_row.append(row)
-            n = 1
+            temp = df.ix[index]
+            group_row = group_row.append(temp)
         else:
             if row['dayofweek'] == old_row['dayofweek']:
                 old_row = row
-                group_row.append(row)
-                n += 1
+                temp = df.ix[index]
+                group_row = group_row.append(temp)
             else:
                 old_row = row
-                new_df.append(sum(group_row))
-                n = 1
-                del temp
+                temp_sum = (group_row.sum(axis=0)) / len(group_row)
+                new_df = new_df.append(temp_sum, ignore_index=True)
+                group_row = pd.DataFrame()
+
     Y = new_df.iloc[:, :1]
     X = new_df.iloc[:, 1:]
     return X, Y
 
 extract(path, col_names)
-#/Users/tnnque/PycharmProjects/data-analytics-ex/que/train_small_cleaned.csv
-# SMA_50 SMA_20 dayofweek
-
-
-    #
-    # for col_name in range(0, len(col_names)):
